77e3cafc1e418d19ae5f8bfac86497b7f850a617
robert-11 <roby9814@yahoo.com>
2020-03-04 18:52:27 +0200

    first exercise

--- Lab 1/src/ro/unitbv/javadatatype/Exercitiu1.java
+++ Lab 1/src/ro/unitbv/javadatatype/Exercitiu1.java
@@ -1,11 +1,31 @@
 package ro.unitbv.javadatatype;
 
+import java.util.Scanner;
+
 public class Exercitiu1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 		System.out.println("Hello World");
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		//afiseaza mesajul in consola
+		System.out.println("Introdu nr 1: ");
+		int nr1 = scanner.nextInt();
+		System.out.println("Introdu nr 2: ");
+		int nr2 = scanner.nextInt();
+				
+		//afiseaza output-ul
+		System.out.println("Nr citit este: " +nr1);
+		System.out.println("Nr citit este: "+nr2);
+		
+		System.out.println("Suma: " +(nr1+nr2));
+		System.out.println("Diferenta: " +(nr1-nr2));
+		System.out.println("Produsul: " +(nr1*nr2));
+		System.out.println("Media: " +((nr1+nr2)/2));
+		System.out.println("Maxim: " + Math.max(nr1, nr2));
+		double impartire = (double)nr1/nr2;
+		System.out.println("Impartirea: " +impartire);
 	}
 
 }
